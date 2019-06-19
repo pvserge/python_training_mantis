442ddda64a31995fe8a8147c643916aaa31ede86
sergepv <serge@ncryptedcloud.com>
2019-06-19 17:09:00 +0300

    Create james user using telnet

--- fixture/application.py
+++ fixture/application.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
+from fixture.james import JamesHelper
 
 class Application:
 
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, config):
         browser_upper = browser.upper()
         if browser_upper == "FIREFOX":
             self.wd = webdriver.Firefox()
@@ -17,7 +18,9 @@ class Application:
         self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.project = ProjectHelper(self)
-        self.base_url = base_url
+        self.james = JamesHelper(self)
+        self.config = config
+        self.base_url = config['web']['baseUrl']
 
     def open_home_page(self):
         wd = self.wd
